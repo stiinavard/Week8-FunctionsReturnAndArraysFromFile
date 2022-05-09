a5606577df8506afa498a49fed91d62eb4ac363a
Stiina Vard <stiinavard@gmail.com>
2022-05-09 21:03:29 +0300

    add new projects

---
+++ ChuckNorris/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace ChuckNorris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string randomJoke;
+            randomJoke = GetRandomFromFile("chuck.txt");
+            Console.WriteLine(randomJoke);
+        }
+        private static int GenerateRandomIndex(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+            return randomIndex;
+        }
+
+        private static string GetRandomFromFile(string fileName)
+        {
+            string filePath = $@"C:\Users\...\samples\chuck.txt";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+            string randomElement = dataFromFile[GenerateRandomIndex(dataFromFile)];
+
+            return randomElement;
+        }
+    }
+}
